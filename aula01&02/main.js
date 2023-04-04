0545015303c9c4f15a64400fde3ac31620160af9
Kaio Marques Fonseca <kaio.fonseca@codeplan.df.gov.br>
2023-04-04 09:00:19 -0300

    aula 1 e metade da 2

---
+++ aula01&02/main.js
@@ -0,0 +1,29 @@
+{/* ------ Diferenças entre as declarações de variáveis ------ */} 
+
+// var = alert('bababoi') --- Var VAR não é muito utilizado hoje em dia para declarar variáveis porque ele não tem Block Scope.
+// Let = alert('bababoi') --- Let tem block scope e é utilizado quando essa variável pode ser mudada no futuro.
+// const = alert('bababoi') --- Const é utilizado quando aquela variável tem que ser constante e não pode mudar.
+
+{/* ------ Strings ------ */} 
+
+// const message = 'Hello Wolrd'
+// Método que diz quantas caractéres que essa string tem. 
+// console.log(message.length)
+
+// const firstName = 'Kaio'
+// const lastName = 'Fonseca'
+// .toUpperCase deixa tudo maiúsculo - .toLowerCase deixa tudo minúsculo
+// console.log(`Meu nome é ${firstName.toUpperCase} ${lastName.toLowerCase}`)
+
+// const names = 'Kaio, Felipe, Juh'
+// console.log(names.split(',')) // split conta a string em cada virgula e cria um array (se n colocar a virgula ele cria um item com cada letra)
+
+{/* ------ Numbers ------ */}
+
+// const number = 5
+// console.log(number)
+// Alguns métodos de números
+// console.log(number.toString()) // Transforma em string
+// console.log(typeof number) // Ve qual é o tipo deessa string
+
+{/* ------ Booleans ------ */}
