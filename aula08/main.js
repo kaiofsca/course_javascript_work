91034488aacf27dd7dd3cf5a83fe9f13bc52e636
Kaio Marques Fonseca <kaio.fonseca@codeplan.df.gov.br>
2023-04-05 11:55:34 -0300

    aula08 parte 1

---
+++ aula08/main.js
@@ -0,0 +1,32 @@
+{/* ------ Condicionais ------ */}
+// Tal funçãosó vai ser executada caso uma condição for true, se retornar false ela retorna outra função.
+
+// Diferença entre "==" e "===" um compara só o valor (==) e o outro compara o valor e o tipo (===).
+
+// Entendendo If, Else if & Else
+const sum = 1 + 1;
+
+if (sum === 2) {
+    console.log('Sum is 2!')
+} else if (sum === 3) {
+    console.log('Sum is 3!')
+} else {
+    console.log('Sum is not 2!')
+}
+
+// Operadores AND (&&) e OR (||)
+
+const sum1 = 2 + 2;
+const sum2 = 3 + 3;
+// No && as duas condições passadas tem que ser verdadeira para a função rodar.
+if (sum1 === 4 && sum2 === 6) {
+    console.log('Sum is correct!')
+}
+// No || se uma das condições passadas for verdadeira a função roda.
+if (sum1 === 4 || sum2 === 6) {
+    console.log('Sum is 4 or sum is 6!')
+}
+
+// Operadores Ternários - Perfeito para definir variáveis baseando em uma condição
+
+
