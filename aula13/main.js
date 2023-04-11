32476128a70b87b99b4f183a45490ee403ede601
Kaio Marques Fonseca <kaio.fonseca@codeplan.df.gov.br>
2023-04-11 10:39:06 -0300

    aula13

---
+++ aula13/main.js
@@ -0,0 +1,16 @@
+{/* ------ Manipulando elementos do DOM ------ */}
+const items = document.querySelector(".items");
+const button = document.querySelector('.btn')
+// - Removendo elementos do DOM -
+// - Remove todos os items -
+
+//items.remove()
+
+// - Removendo items específicos -
+
+//items.firstElementChild.remove()
+//items.lastElementChild.remove()
+//items.children[1].textContent = 'Item um'
+
+// Estilizando elementos DOM
+button.style.background = 'purple';
